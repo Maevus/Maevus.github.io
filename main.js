1b198027a05768240d4949b78e7ea89726f919da
Maevus <maevus@gmail.com>
2021-05-07 23:22:43 +0200

    site contents

---
+++ main.js
@@ -0,0 +1,31 @@
+const selectElement = (s) => document.querySelector(s);
+const navlinks = document.querySelectorAll(".nav-link");
+
+selectElement(".burger").addEventListener("click", () => {
+    selectElement(".nav-list").classList.toggle("active");
+    selectElement(".burger").classList.toggle("toggle");
+
+    navlinks.forEach((link, index) => {
+        if(link.style.animation) {
+            link.style.animation = ""
+        } else {
+            link.style.animation = `navLinkAnimation 0.5s ease forwards ${index/10+0.1}s`
+        }
+    });
+});
+
+
+navlinks.forEach(link => {
+    link.addEventListener("click", () => {
+        selectElement(".nav-list").classList.toggle("active");
+        selectElement(".burger").classList.toggle("toggle");
+
+        navlinks.forEach((link, index) => {
+            if(link.style.animation) {
+                link.style.animation = ""
+            } else {
+                link.style.animation = `navLinkAnimation 0.5s ease forwards ${index/10+0.1}s`
+            }
+        });
+    })
+})
